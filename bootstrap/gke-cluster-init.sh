0b76d79498919157063bf77b0732f233861b1d80
Pipe Martina <fmartina@google.com>
2022-07-14 14:13:34 +0000

    Changed GKE cluster names

--- bootstrap/gke-cluster-init.sh
+++ bootstrap/gke-cluster-init.sh
@@ -7,20 +7,20 @@ if [[ -z "${PROJECT_ID}" ]]; then
 fi
 # Test cluster
 echo "creating testcluster..."
-gcloud beta container --project "$PROJECT_ID" clusters create-auto "testcluster" \
---region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
---subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
+gcloud beta container --project "$PROJECT_ID" clusters create-auto "test-cluster" \
+--region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/k8s-vpc" \
+--subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/k8s-subnet" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async
 # Staging cluster
 echo "creating stagingcluster..."
-gcloud beta container --project "$PROJECT_ID" clusters create-auto "stagingcluster" \
---region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
---subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
+gcloud beta container --project "$PROJECT_ID" clusters create-auto "staging-cluster" \
+--region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/k8s-vpc" \
+--subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/k8s-subnet" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async
 # Prod cluster
 echo "creating prodcluster..."
-gcloud beta container --project "$PROJECT_ID" clusters create-auto "prodcluster" \
---region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/default" \
---subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/default" \
+gcloud beta container --project "$PROJECT_ID" clusters create-auto "prod-cluster" \
+--region "us-central1" --release-channel "regular" --network "projects/$PROJECT_ID/global/networks/k8s-vpc" \
+--subnetwork "projects/$PROJECT_ID/regions/us-central1/subnetworks/k8s-subnet" \
 --cluster-ipv4-cidr "/17" --services-ipv4-cidr "/22" --async
 echo "Creating clusters! Check the UI for progress"
