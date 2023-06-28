4f1af5308b2356d7f87c9bfc9870e4686180f35a
Victor Szalvay <victor.szalvay@gmail.com>
2023-06-28 04:54:23 +0000

    unit test fix

--- app/api_tests.py
+++ app/api_tests.py
@@ -8,8 +8,9 @@ class TestPopulation(unittest.TestCase):
         Test that method returns expected country
         """
         ip_address = "8.8.8.8"
+        expected = "US"
         cc = target.get_country_code_by_ip(ip_address)
-        self.assertEqual(cc, US)    
+        self.assertEqual(cc, expected)    
 
 if __name__ == '__main__':
     unittest.main()
