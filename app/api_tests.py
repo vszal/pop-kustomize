7b1e63d09a4a5a7521e52c97e15957ddd2367ff5
Victor Szalvay <victor.szalvay@gmail.com>
2023-06-28 04:42:40 +0000

    update test for world stats

--- app/api_tests.py
+++ app/api_tests.py
@@ -3,23 +3,13 @@ import unittest
 target = __import__("app")
 
 class TestPopulation(unittest.TestCase):
-    def test_census_data(self):
+    def test_get_country_code_by_ip(self):
         """
-        Test that method returns expected population
-        given lat / long inputs
+        Test that method returns expected country
         """
-        lat = "47.6062"
-        long = "-122.3321"
-        county, population, density = target.get_census_data(lat, long)
-        self.assertEqual(population, 2252782)
-    
-    def test_ip_to_zip(self):
-        """
-        Test that ip to zipcode translation works
-        """
-        test_ip = "8.8.8.8"
-        zipcode, country, lat, lng = target.get_location_by_ip(test_ip)
-        self.assertEqual(zipcode, "20149")
+        ip_address = "8.8.8.8"
+        cc = get_country_code_by_ip(ip_address)
+        self.assertEqual(cc, US)    
 
 if __name__ == '__main__':
     unittest.main()
